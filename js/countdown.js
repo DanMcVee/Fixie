2ffa7dba7bfc6114210ade940f1a40872566d4e5
DanMcVee <96514527+DanMcVee@users.noreply.github.com>
2021-12-22 10:10:36 +0400

    add files

---
+++ js/countdown.js
@@ -0,0 +1,31 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const deadline = new Date(2021, 11, 25);
+ 
+  let timerId = null;
+ 
+ 
+  function countdownTimer() {
+    const diff = deadline - new Date();
+  
+    const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+    const hours =  diff > 0 ? Math.floor( diff / 1000 / 60 / 60) % 24 : 0;
+    const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+    const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+    $days.textContent = days < 10 ? '0' + days : days;
+    $hours.textContent = hours < 10 ? '0' + hours : hours;
+    $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+    $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+    
+  }
+ 
+  const $days = document.querySelector('.timer__days');
+  const $hours = document.querySelector('.timer__hours');
+  const $minutes = document.querySelector('.timer__minutes');
+  const $seconds = document.querySelector('.timer__seconds');
+
+  countdownTimer();
+
+  timerId = setInterval(countdownTimer, 1000);
+});
